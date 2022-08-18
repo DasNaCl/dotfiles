867f468848da34b100bc33be0ca7217c60c5476a
Matthis Kruse <s8makrus@stud.uni-saarland.de>
2022-08-18 22:46:36 +0200

    Add updater script that automatically updates $HOME

---
+++ update.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+configspath="${MY_DOTFILES:-$HOME/dotfiles}"
+
+for file in $(find . -type f -not -path './.git/*' | sed '/.\/update.sh/d' | sed '/.\/.gitignore/d'); do
+  homef="$HOME"/"$file"
+  cfgf="$configspath"/"$file"
+  patchf="$configspath"/"$file.patch"
+  diff "$homef" "$cfgf" > "$patchf"
+  patch "$homef" "$cfgf"
+done
+
+if [ ! -f /etc/local.d/update.start ]; then
+  echo "The script will install itself to /etc/local.d/ so that it gets run at boot."
+  sudo cp "$0" /etc/local.d/update.start
+  sudo chown +x /etc/local.d/update.start
+else
+  diff "$0" /etc/local.d/update.start > "$configspath/update.sh.patch"
+  if cat "$configspath/update.sh.patch" | read -r _USELESSREPLY; then
+    echo "The script will patch the file /etc/local.d/update.start"
+    sudo patch /etc/local.d/update.start "$configspath/update.sh.patch"
+  fi
+fi
+
+
