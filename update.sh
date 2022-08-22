1798a03d4006b52ae06874a0e4409f55c82236f7
Matthis Kruse <s8makrus@stud.uni-saarland.de>
2022-08-22 07:46:13 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,15 +8,20 @@ cd "$configspath"
 for file in $(find . -type f -not -path './.git/*' | sed '/.\/update.sh/d' | sed '/.\/.gitignore/d'); do
   homef="$HOME"/"$file"
   cfgf="$configspath"/"$file"
-  patchf="$configspath"/"$file.patch"
-  diff "$homef" "$cfgf" > "$patchf"
-  patch "$homef" "$cfgf"
+  if [ ! -f "$homef" ]; then
+    cp "$cfgf" "$homef"
+  else
+    patchf="$configspath"/"$file.patch"
+    diff "$homef" "$cfgf" > "$patchf"
+    patch "$homef" "$cfgf"
+    rm "$configspath"/"$file.patch"
+  fi
 done
 
 if [ ! -f /etc/local.d/update.start ]; then
   echo "The script will install itself to /etc/local.d/ so that it gets run at boot."
   sudo cp "$0" /etc/local.d/update.start
-  sudo chown +x /etc/local.d/update.start
+  sudo chmod +x /etc/local.d/update.start
 else
   diff "$0" /etc/local.d/update.start > "$configspath/update.sh.patch"
   if cat "$configspath/update.sh.patch" | read -r _USELESSREPLY; then
