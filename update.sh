ffc45cc678eb9ffa468ce80367b0f706a1a1120c
Matthis Kruse <s8makrus@stud.uni-saarland.de>
2022-08-22 07:59:43 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 configspath="${MY_DOTFILES:-$HOME/dotfiles}"
 
 oldpwd="$PWD"
-cd "$configspath"
+cd "$configspath" || exit
 
 for file in $(find . -type f -not -path './.git/*' | sed '/.\/update.sh/d' | sed '/.\/.gitignore/d'); do
   homef="$HOME"/"$file"
@@ -11,6 +11,7 @@ for file in $(find . -type f -not -path './.git/*' | sed '/.\/update.sh/d' | sed
   echo "home: $homef"
   echo "cfgf: $cfgf"
   if [ ! -f "$homef" ]; then
+    mkdir -p "$(dirname "$(realpath "$homef")")"
     cp "$cfgf" "$homef"
   else
     patchf="$configspath"/"$file.patch"
@@ -22,10 +23,10 @@ done
 
 if [ ! -f /etc/local.d/update.start ]; then
   echo "The script will install itself to /etc/local.d/ so that it gets run at boot."
-  sudo cp "$(realpath "$0")" /etc/local.d/update.start
+  sudo cp "$configspath/update.sh" /etc/local.d/update.start
   sudo chmod +x /etc/local.d/update.start
 else
-  diff "$(realpath "$0")" /etc/local.d/update.start > "$configspath/update.sh.patch"
+  diff "$configspath/update.sh" /etc/local.d/update.start > "$configspath/update.sh.patch"
   if cat "$configspath/update.sh.patch" | read -r _USELESSREPLY; then
     echo "The script will patch the file /etc/local.d/update.start"
     sudo patch /etc/local.d/update.start "$configspath/update.sh.patch"
